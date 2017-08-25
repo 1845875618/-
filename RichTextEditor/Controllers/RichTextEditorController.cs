890f488ed59ff82a51623bd017f8f08d98461e7c
杨剑 <1845875618@qq.com>
2017-08-25 15:17:24 +0800

    commit message

---
+++ RichTextEditor/Controllers/RichTextEditorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RichTextEditor.Controllers
+{
+    public class RichTextEditorController : Controller
+    {
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Index(FormCollection fc)
+        {
+            var content = fc["editor"];
+
+            return View();
+        }
+
+    }
+}
